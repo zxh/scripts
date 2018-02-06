a6e0b08387ebea0cc8136aca0c2a426acb52db7b
zxh <zxholy@gmail.com>
2018-02-06 16:25:49 +0800

    update delete user script, delete user home dir.

--- delete_user.sh
+++ delete_user.sh
@@ -1,7 +1,6 @@
 echo -n "Which user you want to delete? Write it: "
 read delete_user
-userdel $delete_user
-groupdel $delete_user
+userdel -r $delete_user
 echo ""
 echo "Attention!!! If you have give user [$delete_user] root permission, remember to delete content in /etc/sudoers !!!"
 echo ""
