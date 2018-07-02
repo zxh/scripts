16197db3da332dde3c3cb577ab5a165224ef5883
root <root@nx-test-inf-1.cn-northwest-1.compute.internal>
2018-07-02 03:32:49 +0000

    refactor project. add some exec command, add some service scripts

--- import_exec.sh
+++
@@ -1,4 +0,0 @@
-cp datefmt /usr/local/bin/
-cp assh /usr/local/bin/
-cp see /usr/local/bin/
-cp hist /usr/local/bin/
---
+++ service/start_zk.sh
@@ -0,0 +1,4 @@
+sudo su
+cd /var/lib/zookeeper/logs/
+sh /usr/local/zookeeper/bin/zkServer.sh start 
+cd -
