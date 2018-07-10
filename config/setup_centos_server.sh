8a0e83a512fc5db4970b912fd21b9e49588972dd
zxh <zxholy@gmail.com>
2018-07-10 17:38:29 +0800

    create logs, scripts and app path.

--- config/setup_centos_server.sh
+++ config/setup_centos_server.sh
@@ -1,4 +1,8 @@
-yum install wget -y
-yum install java-1.8.0-openjdk-devel.x86_64 -y
-yum install maven -y
-yum install git -y
+sudo yum install wget -y
+sudo yum install java-1.8.0-openjdk-devel.x86_64 -y
+sudo yum install maven -y
+sudo yum install git -y
+sudo mkdir -p /data/app/
+sudo mkdir -p /data/logs/
+sudo mkdir -p /data/scripts/
+
