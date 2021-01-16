a422943825b2349da609c932dd51390988de3a8a
zxh <zxholy@gmail.com>
2021-01-16 11:31:54 +0800

    add some util by default.

--- config/setup_centos_server.sh
+++ config/setup_centos_server.sh
@@ -1,8 +1,8 @@
-sudo yum install wget -y
 sudo yum install java-1.8.0-openjdk-devel.x86_64 -y
-sudo yum install maven -y
-sudo yum install git -y
+sudo yum install git nc htop wget maven vim unzip tig tree xclip xsel -y
 sudo mkdir -p /data/app/
 sudo mkdir -p /data/logs/
 sudo mkdir -p /data/scripts/
+sudo mkdir -p /data/resources/
 
+sudo timedatectl set-timezone Asia/Shanghai
